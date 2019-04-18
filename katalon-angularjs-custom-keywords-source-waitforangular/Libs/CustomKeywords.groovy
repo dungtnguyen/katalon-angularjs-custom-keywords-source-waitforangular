843f23a6605de9765b5fa32e21d1b71b407ea139
dungtnguyen <45087256+dungtnguyen@users.noreply.github.com>
2019-04-18 13:33:42 +0700

    Add files via upload

---
+++ katalon-angularjs-custom-keywords-source-waitforangular/Libs/CustomKeywords.groovy
@@ -0,0 +1,9 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+
+def static "com.katalon.plugin.keyword.waitforangular.WaitForAngularKeywords.waitForAngularLoad"() {
+    (new com.katalon.plugin.keyword.waitforangular.WaitForAngularKeywords()).waitForAngularLoad()
+}
